e4a02e2e24340324b3e4f875d653224986df163a
DSHakirov <shakirovdominatus@gmail.com>
2024-11-28 10:54:21 +0300

    elements admin

--- auto_checklist/checklists/admin.py
+++ auto_checklist/checklists/admin.py
@@ -5,3 +5,9 @@ from .models import Element, Check
 class CheckInline(admin.TabularInline):
     model = Check
     extra = 0
+
+class ElementAdmin(admin.ModelAdmin):
+    list_display = ["element"]
+
+
+admin.site.register(Element, ElementAdmin)
